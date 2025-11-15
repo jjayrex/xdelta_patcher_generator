c4bc84174e5b75d5461ed8e7485e76fe68faa324
jjayrex <jjayrex@gmail.com>
2025-11-15 02:00:12 +0100

    Workspace setup

---
+++ patch_types/src/lib.rs
@@ -0,0 +1,37 @@
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Manifest {
+    pub product: String,
+    pub from_version: String,
+    pub to_version: String,
+    pub files: Vec<FileEntry>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct FileEntry {
+    pub path: String,
+    pub kind: PathKind,
+    pub original_hash: [u8; 32],
+    pub new_hash: [u8; 32],
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PathKind {
+    Unchanged,
+    Patched { idx: usize },
+    Added { idx: usize },
+    Deleted,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum PatchData {
+    Xdelta(Vec<u8>),    // xdelta diff
+    Full(Vec<u8>),      // full file
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PatchBundle {
+    pub manifest: Manifest,
+    pub entries: Vec<PatchData>,
+}
